6c068c8830d9278999df153cf6d5aeb1d05d3462
Shasidharyadav <110014214+Shasidharyadav@users.noreply.github.com>
2023-12-19 21:31:40 +0530

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -1,1 +1,13 @@
-
+w=60
+t=20
+h=30
+W=0.5*t**2-0.2*h+0.1*w-15
+print(W)
+if(W>300):
+    print("Weather is Sunny")
+elif(200<W<=300):
+    print("Weather is Cloudy")
+elif(100<W<=200):
+        print("Weather is Rainy")
+else:
+            print("weather is stormy")
